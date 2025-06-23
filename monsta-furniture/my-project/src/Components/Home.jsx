2594de64cdc26819eeb6f4ae44f6372a7ad2b175
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2025-06-23 18:47:00 +0530

    made siome changes

--- monsta-furniture/my-project/src/Components/Home.jsx
+++ monsta-furniture/my-project/src/Components/Home.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import { FiFileText } from "react-icons/fi";
+import { FaRupeeSign } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
-      
+      <div className='container-fluid mx-auto'>
+        <div className='row'>
+          <div className='columns-1 w-[100%] p-[30px] text-[#37474f] text-[22px]'>Welcome! Admin</div>
+          <div className='columns-4 justify-around flex flex-row w-[100%] h-[120px] gap-1'>
+            <div className='bg-[#3e8ef7] flex  w-[23%] rounded p-5 box-border'>
+              <div className='w-[80%] text-white box-border'>
+                <h2 className='text-[30px]'>0</h2>
+                <span className='text-[12px]'>TOTAL ORDERS</span>
+              </div>
+              <div className='w-[20%] text-[40px] text-gray-700 pt-[35px] ps-[25px]'>
+                <FiFileText />
+              </div>
+            </div>
+            <div className='bg-[#17b3a3] h-[100%] w-[23%] rounded p-5 box-border flex'>
+              <div className='w-[80%] text-white box-border'>
+                <h2 className='text-[30px]'>0</h2>
+                <span className='text-[12px]'>TOTAL USERS</span>
+              </div>
+              <div className='w-[20%] text-[40px] text-gray-700 pt-[35px] ps-[25px]'>
+                <FiFileText />
+              </div>
+            </div>
+            <div className='bg-[#0bb2d4] h-[100%] w-[23%] rounded p-5 box-border flex'>
+            <div className='w-[80%] text-white box-border'>
+                <h2 className='text-[30px]'>&#8377;0</h2>
+                <span className='text-[12px]'>TOTAL EARNINGS</span>
+              </div>
+              <div className='w-[20%] text-[40px] text-gray-700 pt-[35px] ps-[25px]'>
+              <FaRupeeSign />
+              </div>
+            </div>
+            <div className='bg-[#9463f7] h-[100%] w-[23%] rounded p-5 box-border flex'>
+            <div className='w-[80%] text-white box-border'>
+                <h2 className='text-[30px]'>0</h2>
+                <span className='text-[12px]'>TOTAL PRODUCTS</span>
+              </div>
+              <div className='w-[20%] text-[40px] text-gray-700 pt-[35px] ps-[25px]'>
+                <FiFileText />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- monsta-furniture/my-project/src/Components/SideBarr.jsx
+++ monsta-furniture/my-project/src/Components/SideBarr.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems, SidebarLogo, SidebarCollapse } from "flowbite-react";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import { HiUsers,HiLocationMarker } from "react-icons/hi";
+import { PiNotebookLight } from "react-icons/pi";
+import { VscSettings } from "react-icons/vsc";
+import { IoLogoWechat } from "react-icons/io5";
+import { FiFileText } from "react-icons/fi";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+
+
+
+// import { HiUsers } from "react-icons/hi";
 
 export default function SideBarr() {
     return (
@@ -8,29 +17,41 @@ export default function SideBarr() {
             <Sidebar aria-label="Sidebar with multi-level dropdown example">
                 <SidebarItems>
                     <SidebarItemGroup>
-                        <SidebarItem href="#" icon={HiChartPie}>
+                        <SidebarItem href="/" icon={MdOutlineSpaceDashboard}>
                             Dashboard
                         </SidebarItem>
-                        <SidebarCollapse icon={HiShoppingBag} label="E-commerce">
-                            <SidebarItem href="#">Products</SidebarItem>
-                            <SidebarItem href="#">Sales</SidebarItem>
-                            <SidebarItem href="#">Refunds</SidebarItem>
-                            <SidebarItem href="#">Shipping</SidebarItem>
+                        <SidebarCollapse icon={HiUsers} label="User Management">
+                            <SidebarItem href="/">Admins</SidebarItem>
+                            <SidebarItem href="/">Users</SidebarItem>
                         </SidebarCollapse>
-                        <SidebarItem href="#" icon={HiInbox}>
-                            Inbox
+                        <SidebarCollapse icon={HiLocationMarker} label="Locations">
+                            <SidebarItem href="/">Countries</SidebarItem>
+                        </SidebarCollapse>
+                        <SidebarCollapse icon={PiNotebookLight} label="Master Catalogs">
+                            <SidebarItem href="/">Sliders</SidebarItem>
+                            <SidebarItem href="/">Testimonials</SidebarItem>
+                            <SidebarItem href="/">Why Choose Us</SidebarItem>
+                            <SidebarItem href="/">Coupons</SidebarItem>
+                        </SidebarCollapse>
+                        <SidebarCollapse icon={PiNotebookLight} label="Product Catalogs">
+                            <SidebarItem href="/">Catagories</SidebarItem>
+                            <SidebarItem href="/">Materials</SidebarItem>
+                            <SidebarItem href="/">Colors</SidebarItem>
+                            <SidebarItem href="/">Products</SidebarItem>
+                        </SidebarCollapse>
+                        <SidebarCollapse icon={VscSettings} label="Enquirys">
+                            <SidebarItem href="/">Contact Enquirys</SidebarItem>
+                            <SidebarItem href="/">Newsletters</SidebarItem>
+                        </SidebarCollapse>
+                        <SidebarCollapse icon={VscSettings} label="Configuration">
+                            <SidebarItem href="/">Payment Gateways</SidebarItem>
+                            <SidebarItem href="/">Configurations</SidebarItem>
+                        </SidebarCollapse>
+                        <SidebarItem href="/" icon={IoLogoWechat}>
+                            FAQs
                         </SidebarItem>
-                        <SidebarItem href="#" icon={HiUser}>
-                            Users
-                        </SidebarItem>
-                        <SidebarItem href="#" icon={HiShoppingBag}>
-                            Products
-                        </SidebarItem>
-                        <SidebarItem href="#" icon={HiArrowSmRight}>
-                            Sign In
-                        </SidebarItem>
-                        <SidebarItem href="#" icon={HiTable}>
-                            Sign Up
+                        <SidebarItem href="/" icon={FiFileText}>
+                            CMS Pages
                         </SidebarItem>
                     </SidebarItemGroup>
                 </SidebarItems>
