831b9efb64fafd9bc0377feeb7134de30c8892da
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2025-07-22 10:09:47 +0530

    added some UI on homepage

--- Monsta-furniture(website)/monsta_furniture_website/src/app/Components/Common/Header.jsx
+++ Monsta-furniture(website)/monsta_furniture_website/src/app/Components/Common/Header.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { IoIosSearch,IoMdCart,IoIosArrowDown } from "react-icons/io";
 import { FaHeart } from "react-icons/fa";
 
 
 
 export default function Header() {
+
+    let [livingToggle, setLivingToggle] = useState(false);
+    let [sofaToggle, setSofaToggle] = useState(false);
+    let [pagesToggle, setPagesToggle] = useState(false);
+
+    const livingDropdown = () => {
+        var bool = !livingToggle;
+        setLivingToggle(bool);
+    }
+
+    const sofaDropdown = () => {
+        var bool = !sofaToggle;
+        setSofaToggle(bool);
+    }
+
+    const pagesDropdown = () => {
+        var bool = !pagesToggle;
+        setPagesToggle(bool);
+    }
+    // console.log(livingToggle);
   return (
     <>
       <div className='w-screen'>
@@ -19,17 +40,17 @@ export default function Header() {
                 <div className='border border-2'>
                     <img src="/HomePageImages/logo.png" alt="" className='w-[150px] h-[40px]' />
                 </div>
-                <div className='hover:text-[#C09578] cursor-pointer flex gap-5'>
+                <div className='cursor-pointer flex gap-5'>
                     <div className='border border-gray-300 h-[40px] w-[265px] flex justify-between items-center px-2'>
-                        <input type="text" placeholder='Search Product...'  />
+                        <input type="text" placeholder='Search Product...' className='hover:text-[#C09578]' />
                         <IoIosSearch className='text-xl'/>
                     </div>
-                    <div className='cursor-pointer border border-gray-300 rounded flex justify-center items-center px-3 text-xl'>
+                    <div className='cursor-pointer hover:text-[#C09578] border border-gray-300 rounded flex justify-center items-center px-3 text-xl'>
                         <button>
                             <FaHeart />
                         </button>
                     </div>
-                    <div className='border border-gray-300 rounded flex justify-center items-center px-3 gap-2 text-lg relative'>
+                    <div className='border border-gray-300 rounded flex justify-center items-center px-3 gap-2 text-lg relative hover:text-[#C09578]'>
                         <button className='rounded-[50%] w-[20px] h-[20px] bg-[#C09578] text-white text-xs absolute top-[10px] left-[-10px]'>1</button>
                         <IoMdCart/>
                         <div className='flex gap-1 justify-center items-center border-s border-gray-300 ps-2 font-bold text-sm'>Rs. 6000 
@@ -43,14 +64,98 @@ export default function Header() {
             <div className='max-w-[1320px] h-[65px] mx-auto flex justify-center items-center text-sm'>
                 <ul className='uppercase flex gap-10 text-gray-600 font-semibold text-sm'>
                     <li className='hover:text-[#C09578] cursor-pointer'>Home</li>
-                    <li className='flex gap-1 items-center hover:text-[#C09578] cursor-pointer'>Living <IoIosArrowDown /></li>
-                    <li className='flex gap-1 items-center hover:text-[#C09578] cursor-pointer'>Sofa <IoIosArrowDown /></li>
-                    <li className='flex gap-1 items-center hover:text-[#C09578] cursor-pointer'>Pages <IoIosArrowDown /></li>
+                    <li className='flex gap-1 items-center hover:text-[#C09578] cursor-pointer' onMouseEnter={livingDropdown} onMouseLeave={livingDropdown}>Living <IoIosArrowDown /></li>
+                    <li className='flex gap-1 items-center hover:text-[#C09578] cursor-pointer' onMouseEnter={sofaDropdown} onMouseLeave={sofaDropdown}>Sofa <IoIosArrowDown /></li>
+                    <li className='flex gap-1 items-center hover:text-[#C09578] cursor-pointer' onMouseEnter={pagesDropdown} onMouseLeave={pagesDropdown}>Pages <IoIosArrowDown /></li>
                     <li className='hover:text-[#C09578] cursor-pointer'>Contact Us</li>
                 </ul>
             </div>
         </div>
       </div>
+      {
+        livingToggle
+        ?
+        <div className='absolute top-[21%] z-10 left-[42%] bg-white p-8 flex gap-8'>
+            <div>
+                <h3 className='hover:text-[#C09578] pb-5 text-base font-bold text-gray-700'>TABLES</h3>
+                <ul>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Side And End Tables</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Nest Of Tables</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Console Table</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Coffee Table Sets</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Coffee Tables</li>
+                </ul>
+            </div>
+            <div>
+                <h3 className='hover:text-[#C09578] pb-5 text-base font-bold text-gray-700'>LIVING STORAGE</h3>
+                <ul>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Prayer Units</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Display Unit</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Shoe Racks</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Chest Of Drawers</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Cabinets And Sideboards</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Bookshelves</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>TV Units</li>
+                </ul>
+            </div>
+            <div>
+                <h3 className='hover:text-[#C09578] pb-5 text-base font-bold text-gray-700'>MIRRORS</h3>
+                <ul>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Wooden Mirrors</li>
+                </ul>
+            </div>
+        </div>
+        :
+        ''
+      }
+
+      {
+        sofaToggle
+        ?
+        <div className='absolute top-[21%] z-10 left-[47%] bg-white p-8 flex gap-8'>
+            <div>
+                <h3 className='hover:text-[#C09578] pb-5 text-base font-bold text-gray-700'>SOFA CUM BED</h3>
+                <ul>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Wooden Sofa Cum Bed</li>
+                </ul>
+            </div>
+            <div>
+                <h3 className='hover:text-[#C09578] pb-5 text-base font-bold text-gray-700'>SOFA SETS</h3>
+                <ul>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>L Shape Sofa</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>1 Seater Sofa</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>2 Seater Sofa</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>3 Seater Sofa</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Wooden Sofa Sets</li>
+                </ul>
+            </div>
+            <div>
+                <h3 className='hover:text-[#C09578] pb-5 text-base font-bold text-gray-700'>SWING JHULA</h3>
+                <ul>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Wooden Jhula</li>
+                </ul>
+            </div>
+        </div>
+        :
+        ''
+      }
+
+      {
+        pagesToggle
+        ?
+        <div className='absolute top-[21%] z-10 left-[52.5%] bg-white p-8 flex gap-8'>
+            <div>
+                <ul>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>About Us</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Cart</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Checkout</li>
+                    <li className='hover:text-[#C09578] text-gray-400 text-sm pb-2'>Frequently Questions</li>
+                </ul>
+            </div>
+        </div>
+        :
+        ''
+      }
     </>
   )
 }
--- Monsta-furniture(website)/monsta_furniture_website/src/app/Components/Home.jsx
+++ Monsta-furniture(website)/monsta_furniture_website/src/app/Components/Home.jsx
@@ -4,56 +4,104 @@ import React, { useState } from 'react'
 export default function Home() {
 
 
-    let [bannerToggle , setBannerToggle] = useState(true);
+  let [bannerToggle, setBannerToggle] = useState(true);
 
-    const toggleBannerr = (v) => {
+  const toggleBannerr = (v) => {
 
-        if(v == 2){
-            setBannerToggle(false);
-        }else{
-            setBannerToggle(true);
-        }
-        
+    if (v == 2) {
+      setBannerToggle(false);
+    } else {
+      setBannerToggle(true);
     }
+
+  }
 
   return (
     <>
       <div className='w-screen relative'>
         {
-            bannerToggle
+          bannerToggle
             ?
             <img src="/HomePageImages/homeBanner1.jpg" alt="Home Banner" />
             :
             <img src="/HomePageImages/homeBanner2.jpg" alt="Home Banner" />
         }
-        
+
         <div className='flex items-center gap-2 absolute bottom-8 left-[50%]'>
-           
-           { bannerToggle
+
+          {bannerToggle
             ?
             <>
-                <button className='rounded-[50%] w-[15px] h-[15px] bg-[#C09578] cursor-pointer' onClick={() => toggleBannerr(1)}></button>
-                <button className='rounded-[50%] w-[10px] h-[10px] bg-white cursor-pointer' onClick={() => toggleBannerr(2)}></button>
+              <button className='rounded-[50%] w-[15px] h-[15px] bg-[#C09578] cursor-pointer' onClick={() => toggleBannerr(1)}></button>
+              <button className='rounded-[50%] w-[10px] h-[10px] bg-white cursor-pointer' onClick={() => toggleBannerr(2)}></button>
             </>
             :
             <>
-                <button className='rounded-[50%] w-[10px] h-[10px] bg-white cursor-pointer' onClick={() => toggleBannerr(1)}></button>
-                <button className='rounded-[50%] w-[15px] h-[15px] bg-[#C09578] cursor-pointer' onClick={() => toggleBannerr(2)}></button>
+              <button className='rounded-[50%] w-[10px] h-[10px] bg-white cursor-pointer' onClick={() => toggleBannerr(1)}></button>
+              <button className='rounded-[50%] w-[15px] h-[15px] bg-[#C09578] cursor-pointer' onClick={() => toggleBannerr(2)}></button>
             </>
-            }
-            
-            
+          }
+
+
         </div>
       </div>
-      
+
+      <div className='w-screen border-b border-gray-200'>
+        <div className='w-[1320px] mx-auto flex justify-center items-center gap-6 py-10'>
+          <div className='w-[33%]  overflow-hidden relative'>
+            <img src="/HomePageImages/yellowchair.webp" alt="" className='w-[100%] hover:scale-110 duration-500' />
+            <div className=' p-8 absolute top-0 left-0 hover:bg-black-100'>
+              <h3 className='text-sm'>Design Creative</h3>
+              <h2 className='text-2xl font-bold'>Chair Collection</h2>
+            </div>
+          </div>
+          <div className='w-[33%] overflow-hidden relative'>
+            <img src="/HomePageImages/redchair.webp" alt="" className='w-[100%] hover:scale-110 duration-500' />
+            <div className=' p-8 absolute top-0 left-0 hover:bg-black-100'>
+              <h3 className='text-sm'>Bestselling Products</h3>
+              <h2 className='text-2xl font-bold'>Chair Collection</h2>
+            </div>
+          </div>
+          <div className='w-[33%] overflow-hidden relative'>
+            <img src="/HomePageImages/yellowchair.webp" alt="" className='w-[100%] hover:scale-110 duration-500' />
+            <div className=' p-8 absolute top-0 left-0 hover:bg-black-100'>
+              <h3 className='text-sm'>Onsale Products </h3>
+              <h2 className='text-2xl font-bold'>Chair Collection</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='w-screen py-10'>
+        <div className='w-[1320px] mx-auto'>
+          <h2 className='text-center mx-auto text-2xl font-bold'>What Our Customers Say ?</h2>
+          <div className='w-[75%] text-center mx-auto text-gray-600 leading-7 my-6'>
+            <p>These Guys have been absolutely outstanding. Perfect Themes and the best of all that you have many options to choose! Best Support team ever! Very fast responding! Thank you very much! I highly recommend this theme and people!</p>
+          </div>
+          <div className='w-[10%] mx-auto flex flex-col gap-3'>
+            <div className='w-[95%] mx-auto rounded-[50%]'>
+              <img src="/HomePageImages/kathyyoung.jpg" alt="" className='mx-auto' />
+            </div>
+            <h2 className='uppercase text-center font-bold text-lg text-gray-700'>Kathy Young</h2>
+            <h3 className='text-gray-500 text-sm text-center'>CEO of SunPark</h3>
+            <p className='text-gray-500 text-sm text-center'>stars</p>
+          </div>
+        </div>
+        <div className='w-[10%] flex justify-center items-center'>
+          <div className='w-[15px] h-[15px] rounded-50% bg-gray'></div>
+          <div className='w-[15px] h-[15px] rounded-50% bg-gray'></div>
+          <div className='w-[15px] h-[15px] rounded-50% bg-gray'></div>
+        </div>
+      </div>
+
       <div className='w-screen bg-[#F8F9F9] flex flex-col gap-8 items-center justify-center py-[5%]'>
         <div className='text-center flex flex-col gap-3 items-center justify-center'>
-            <h3 className='font-bold text-3xl'>Our Newsletter</h3>
-            <p>Get E-mail updates about our latest shop and special offers.</p>
+          <h3 className='font-bold text-3xl'>Our Newsletter</h3>
+          <p>Get E-mail updates about our latest shop and special offers.</p>
         </div>
         <div className='w-[40%] flex justify-center'>
-            <input type="text" placeholder='Email Address...' className='rounded border border-gray-300 p-2 px-3 w-[65%]'/>
-            <button className='bg-[#C09578] rounded p-2 px-3 text-white font-bold w-[25%]'>Subscribe</button>
+          <input type="text" placeholder='Email Address...' className='rounded border border-gray-300 p-2 px-3 w-[65%]' />
+          <button className='bg-[#C09578] rounded p-2 px-3 text-white font-bold w-[25%]'>Subscribe</button>
         </div>
       </div>
     </>
