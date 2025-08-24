b4cb3a82305ec226623455bfe4b50a446d595d69
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2025-08-24 14:14:14 +0530

    Built user-profile, updateProfile,change-password,forgot-password, and reset password APIs and used nodemailer package for mail

--- express-mongoose/src/controller/website/user.controller.js
+++ express-mongoose/src/controller/website/user.controller.js
@@ -2,6 +2,7 @@ const user = require('../../models/user.js');
 var jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const nodemailer = require("nodemailer");
 
 
 exports.register = async (req, res) => {
@@ -118,236 +119,273 @@ exports.login = async (req, res) => {
 
 }
 
+exports.myProfile = async (req, res) => {
+    // console.log(req.headers.authorization);
+    var token = req.headers.authorization;
+    jwt.verify(token, process.env.JWT_SECRET_KEY, function(err, decoded) {
+        if(err){
+            const output = {
+                _status: false,
+                _message: 'Invalid Token !!',
+                _data: err
+            }
+            res.send(output);
+        }else{
+            const output = {
+                _status: true,
+                _message: 'Token Verified !!',
+                _data: decoded.data
+            }
+            res.send(output);
+        }
+    });
 
-// exports.view = async (req, res) => {
-
-//     // const condition = {
-//     //     deleted_at: null
-//     // }
-
-//     // if (req.body != undefined) {
-//     //     if (req.body.name) {
-//     //         condition.name = req.body.name;
-//     //     }
-//     // }
-
-
-//     var limit = 5;
-//     var page = 1;
-//     var skip = 0;
-
-//     if (req.body != undefined) {
-//         limit = req.body.limit ?? limit;
-
-//         page = req.body.page ?? 1;
-
-//         skip = (page - 1) * limit;
-//     }
+}
 
 
+exports.updateProfile = async (req, res) => {
+    // console.log(req.headers.authorization);
+    var token = req.headers.authorization;
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async function(err, decoded) {
+        if(err){
+            const output = {
+                _status: false,
+                _message: 'Invalid Token !!',
+                _data: err
+            }
+            res.send(output);
+        }else{
 
-//     const addCondition = [
-//         {
-//             deleted_at: null,
-//         }
-//     ];
+            saveData = {};
 
-//     const orCondition = [];
+            if(req.body.name != undefined && req.body.name != ''){
+                saveData.name = req.body.name;
+            }
+            if(req.body.mobile_number != undefined && req.body.mobile_number != ''){
+                saveData.mobile_number = req.body.mobile_number;
+            }
+            // if(req.file.filename != undefined && req.file.filename != ''){
+            //     saveData.image = req.file.filename; 
+            // }
 
-    
-//     if (req.body != undefined) {
-//         if (req.body.name != undefined) {
-//             if (req.body.name != '') {
-//                 var name = new RegExp(req.body.name, 'i');
-//                 orCondition.push({ name: name},{ code : name })
-//             }
-//         }
-//     }
+            await user.updateOne({
+                _id : decoded.data._id
+            },{
+                $set : saveData
+            })
+            .then( (result) => {
+                const output = {
+                _status : true,
+                _message : 'Profile Updated Succesfully !!',    
+                _data : result
+                }
+                res.send(output);
+            
+            })
+            .catch( (error) => {
 
-//     if (addCondition.length > 0) {
-//         var filter = { $and: addCondition }
-//     } else {
-//         var filter = {}
-//     }
+                var errorMessage = [];
 
-//     if (orCondition.length > 0) {
-//         filter.$or = orCondition;   
-//     }
+                for(index in error.errors){
+                    errorMessage.push(error.errors[index].message);
+                }
 
-//     // console.log(filter);
+                const output = {
+                    _status : false,
+                    _message : 'Something Went Wrong !!',
+                    _data : errorMessage
+                }
+                res.send(output);
+            })
+        }
+    });
 
-//     var totalRecords = await color.find(filter).countDocuments();
+}
 
-//     await color.find(filter).select('_id name code order status').sort({
-//         _id: 'descending'
-//     })
-//         .limit(limit).skip(skip)
-//         .then((result) => {
-//             if (result.length > 0) {
-//                 const output = {
-//                     _status: true,
-//                     _message: 'Record Found !!',
-//                     _pagination: {
-//                         total_records: totalRecords,
-//                         current_page: page,
-//                         total_pages: Math.ceil(totalRecords / limit)
-//                     },
-//                     _data: result
-//                 }
-//                 res.send(output);
-//             } else {
+exports.changePassword = async (req, res) => {
+    console.log(req.headers.authorization);
+    var token = req.headers.authorization;
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async function(err, decoded) {
+        if(err){
+            const output = {
+                _status: false,
+                _message: 'Invalid Token !!',
+                _data: err
+            }
+            res.send(output);
+        }else{
+            const password = await bcrypt.compare(req.body.current_password, decoded.data.password);
+            if(password){
+                if(req.body.new_password != req.body.confirm_password){
+                    const output = {        
+                        _status : false,
+                        _message : 'New Password and Confirm Password does not match !!',   
+                        _data : []
+                    }
+                    res.send(output);
+                }else{
+                    const password = await bcrypt.hash(req.body.new_password, saltRounds);
+
+                    await user.updateOne({
+                        _id : decoded.data._id
+                    },{
+                        $set : { password : password }
+                    })
+                    .then( (result) => {
+                        const output = {
+                        _status : true,
+                        _message : 'Password Changed Succesfully !!',    
+                        _data : result
+                        }
+                        res.send(output);
+                    
+                    })
+                    .catch( (error) => {
+
+                        var errorMessage = [];
+
+                        for(index in error.errors){
+                            errorMessage.push(error.errors[index].message);
+                        }
+
+                        const output = {
+                            _status : false,
+                            _message : 'Something Went Wrong !!',
+                            _data : errorMessage
+                        }
+                        res.send(output);
+                    })
+                }
+            }else{
+                const output = {        
+                    _status : false,
+                    _message : 'Invalid Current Password !!',   
+                    _data : []
+                }
+                res.send(output);
+            }
+        }
+    });
+}
+
+exports.forgotPassword = async (req, res) => {
+    // console.log(req.headers.authorization);
+    var token = req.headers.authorization;
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async function(err, decoded) {
+        if(err){
+            const output = {
+                _status: false,
+                _message: 'Invalid Token !!',
+                _data: err
+            }
+            res.send(output);
+        }else{
+
+            existingUser = await user.findOne({ email: req.body.email, deleted_at: null });
+            if (!existingUser) {
+                const output = {
+                    _status: false,     
+                    _message: 'Email not registered !!',   
+                    _data: []
+                }
+                res.send(output);
+            }else{
+                token = jwt.sign({ data: existingUser }, process.env.JWT_SECRET_KEY);
+                const transporter = nodemailer.createTransport({
+                    host: "smtp.gmail.com",
+                    port: 587,
+                    secure: false, // true for 465, false for other ports
+                    auth: {
+                        user: process.env.ADMIN_EMAIL,
+                        pass: process.env.ADMIN_PASSWORD,
+                    },
+                });     
+
+                const info = await transporter.sendMail({
+                    from: '"Monsta Furniture" <nareshvaishnav69@gmailcom>',
+                    to: req.body.email, // list of receivers
+                    subject: "Forgot Password",
+                    // text: "Hello world?", // plain‑text body
+                    html: "<b>Hello User, Your Link to Reset Passsword is listed below. Click the link to reset password</br></br></br><a href='http://localhost:3000/reset-password/"+token+"' > Click Here</a></b>", // html body
+
+                }).then((info) => {
+                    const output = {
+                        _status: true,
+                        _message: 'Reset Password link sent to your email, Please check !!',    
+                        _data: info
+                    }   
+                    res.send(output);
+                }).catch((error) => {
+                    const output = {
+                        _status: false,
+                        _message: 'Something Went Wrong !!',    
+                        _data: error
+                    }   
+                    res.send(output);
+                });
+            }
+        }
+    });
+
+}
+
+exports.resetPassword = async (req, res) => {
+    // console.log(req.headers.authorization);
+    var token = req.headers.authorization;
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async function(err, decoded) {
+        if(err){
+            const output = {
+                _status: false,
+                _message: 'Invalid Token !!',
+                _data: err
+            }
+            res.send(output);
+        }else{
+            
+                if(req.body.new_password != req.body.confirm_password){
+                    const output = {        
+                        _status : false,
+                        _message : 'New Password and Confirm Password does not match !!',   
+                        _data : []
+                    }
+                    res.send(output);
+                }else{
+                    const password = await bcrypt.hash(req.body.new_password, saltRounds);
+
+                    await user.updateOne({
+                        _id : decoded.data._id
+                    },{
+                        $set : { password : password }
+                    })
+                    .then( (result) => {
+                        const output = {
+                        _status : true,
+                        _message : 'Password Changed Succesfully !!',    
+                        _data : result
+                        }
+                        res.send(output);
+                    
+                    })
+                    .catch( (error) => {
+
+                        var errorMessage = [];
+
+                        for(index in error.errors){
+                            errorMessage.push(error.errors[index].message);
+                        }
+
+                        const output = {
+                            _status : false,
+                            _message : 'Something Went Wrong !!',
+                            _data : errorMessage
+                        }
+                        res.send(output);
+                    })
+                }
+            
+        }
+    });
+}
 
 
-//                 const output = {
-//                     _status: false,
-//                     _message: 'No Record Found !!',
-//                     _data: result
-//                 }
-//                 res.send(output);
-//             }
-//         })
-//         .catch((error) => {
-//             const output = {
-//                 _status: false,
-//                 _message: 'Something Went Wrong !!',
-//                 _data: error
-//             }
-//             res.send(output);
-//         })
-// }
 
-// exports.update = async (req, res) => {
-
-//     await color.updateOne({
-//         _id: req.params.id
-//     }, {
-//         $set: req.body
-//     })
-
-//         .then((result) => {
-//             const output = {
-//                 _status: true,
-//                 _message: 'Updated Succesfully !!',
-//                 _data: result
-//             }
-//             res.send(output);
-//         })
-//         .catch((error) => {
-
-//             var errorMessage = [];
-
-//             for (index in error.errors) {
-//                 errorMessage.push(error.errors[index].message);
-//             }
-
-//             const output = {
-//                 _status: false,
-//                 _message: 'Something Went Wrong !!',
-//                 _data: errorMessage
-//             }
-//             res.send(output);
-//         })
-// }
-
-// exports.details = async (req, res) => {
-//     // await color.findOne({
-//     //     _id : req.params.id
-//     // })
-
-//     await color.findById(req.params.id)
-//         .then((result) => {
-//             if (result) {
-//                 const output = {
-//                     _status: true,
-//                     _message: 'Record Found !!',
-//                     _data: result
-//                 }
-//                 res.send(output);
-//             } else {
-//                 const output = {
-//                     _status: false,
-//                     _message: 'No Record Found !!',
-//                     _data: result
-//                 }
-//                 res.send(output);
-//             }
-//         })
-//         .catch((error) => {
-//             const output = {
-//                 _status: false,
-//                 _message: 'Something Went Wrong !!',
-//                 _data: error
-//             }
-//             res.send(output);
-//         })
-// }
-
-// exports.changeStatus = async (req, res) => {
-//     await color.updateMany({
-//         _id: req.body.id
-//     }, [{
-//         $set: {
-//             status : {
-//                $not : '$status'
-//             }
-//         }
-//     }])
-
-//         .then((result) => {
-//             const output = {
-//                 _status: true,
-//                 _message: 'Status Changed Succesfully !!',
-//                 _data: result
-//             }
-//             res.send(output);
-//         })
-//         .catch((error) => {
-
-//             var errorMessage = [];
-
-//             for (index in error.errors) {
-//                 errorMessage.push(error.errors[index].message);
-//             }
-
-//             const output = {
-//                 _status: false,
-//                 _message: 'Something Went Wrong !!',
-//                 _data: errorMessage
-//             }
-//             res.send(output);
-//         })
-// }
-
-// exports.destroy = async (req, res) => {
-//     await color.updateMany({
-//         _id: req.body.id
-//     }, {
-//         $set: {
-//             deleted_at: Date.now()
-//         }
-//     })
-
-//         .then((result) => {
-//             const output = {
-//                 _status: true,
-//                 _message: 'Record Deleted Succesfully !!',
-//                 _data: result
-//             }
-//             res.send(output);
-//         })
-//         .catch((error) => {
-
-//             var errorMessage = [];
-
-//             for (index in error.errors) {
-//                 errorMessage.push(error.errors[index].message);
-//             }
-
-//             const output = {
-//                 _status: false,
-//                 _message: 'Something Went Wrong !!',
-//                 _data: errorMessage
-//             }
-//             res.send(output);
-//         })
-// }
