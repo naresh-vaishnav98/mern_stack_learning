17340273082b728a10c37867b8f1447ab2fdd92e
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2025-09-14 09:43:28 +0530

    installed google font to the next js project

--- Monsta-furniture(website)/monsta_furniture_website/src/app/layout.js
+++ Monsta-furniture(website)/monsta_furniture_website/src/app/layout.js
@@ -1,12 +1,22 @@
 import "../../public/css/style.css";
 import Footer from "./Components/Common/Footer";
 import Header from "./Components/Common/Header";
-import { Geist } from 'next/font/google'
+// import { Geist } from 'next/font/google'
+import "@fontsource/playfair-display"; // Defaults to 400 weight
+import "@fontsource/playfair-display/700.css"; // Specific weight
+import { Playfair_Display } from "next/font/google";
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "700"], // choose weights you need
+  variable: "--font-playfair",
+});
 
 
-const geist = Geist({
-  subsets: ['Rubic'],
-})
+
+// const geist = Geist({
+//   subsets: ['Rubic'],
+// })
 
 
 
@@ -14,7 +24,7 @@ const geist = Geist({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={geist.className}>
+    <html lang="en" className={`${playfair.variable}`}>
       <body>
         <Header/>
         {children}
